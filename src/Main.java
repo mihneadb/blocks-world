6e1b97085323544b0e2d4900121c085d86c2ad54
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2015-03-29 17:56:31 +0300

    Default to a simple setup.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
 
-    static boolean DEBUG = false;
+    static boolean DEBUG = true;
 
     public static void main(String[] args) throws Exception {
         Block A = new Block("A");
@@ -13,52 +13,54 @@ public class Main {
         Block F = new Block("F");
         Block G = new Block("G");
 
-//        ArrayList<Predicate> world = new ArrayList<Predicate>(Arrays.asList(
-//                new Predicate(Predicate.ON, B, A),
-//                new Predicate(Predicate.ONTABLE, A),
-//                new Predicate(Predicate.ONTABLE, C),
-//                new Predicate(Predicate.ONTABLE, D),
-//                new Predicate(Predicate.CLEAR, B),
-//                new Predicate(Predicate.CLEAR, C),
-//                new Predicate(Predicate.CLEAR, D),
-//                new Predicate(Predicate.ARMEMPTY)
-//        ));
-//
-//        ArrayList<Predicate> targetWorld = new ArrayList<Predicate>(Arrays.asList(
-//                new Predicate(Predicate.ON, C, A),
-//                new Predicate(Predicate.ON, B, D),
-//                new Predicate(Predicate.ONTABLE, A),
-//                new Predicate(Predicate.ONTABLE, D),
-//                new Predicate(Predicate.CLEAR, C),
-//                new Predicate(Predicate.CLEAR, B),
-//                new Predicate(Predicate.ARMEMPTY)
-//        ));
-
+        // Simple setup.
         ArrayList<Predicate> world = new ArrayList<Predicate>(Arrays.asList(
+                new Predicate(Predicate.ON, B, A),
+                new Predicate(Predicate.ONTABLE, A),
                 new Predicate(Predicate.ONTABLE, C),
-                new Predicate(Predicate.ONTABLE, G),
-                new Predicate(Predicate.CLEAR, A),
+                new Predicate(Predicate.ONTABLE, D),
+                new Predicate(Predicate.CLEAR, B),
+                new Predicate(Predicate.CLEAR, C),
                 new Predicate(Predicate.CLEAR, D),
-                new Predicate(Predicate.ON, A, B),
-                new Predicate(Predicate.ON, B, C),
-                new Predicate(Predicate.ON, D, E),
-                new Predicate(Predicate.ON, E, F),
-                new Predicate(Predicate.ON, F, G),
                 new Predicate(Predicate.ARMEMPTY)
         ));
 
         ArrayList<Predicate> targetWorld = new ArrayList<Predicate>(Arrays.asList(
-                new Predicate(Predicate.ONTABLE, A),
-                new Predicate(Predicate.ONTABLE, F),
-                new Predicate(Predicate.CLEAR, B),
-                new Predicate(Predicate.CLEAR, E),
+                new Predicate(Predicate.ON, C, A),
                 new Predicate(Predicate.ON, B, D),
-                new Predicate(Predicate.ON, D, G),
-                new Predicate(Predicate.ON, G, A),
-                new Predicate(Predicate.ON, E, C),
-                new Predicate(Predicate.ON, C, F),
+                new Predicate(Predicate.ONTABLE, A),
+                new Predicate(Predicate.ONTABLE, D),
+                new Predicate(Predicate.CLEAR, C),
+                new Predicate(Predicate.CLEAR, B),
                 new Predicate(Predicate.ARMEMPTY)
         ));
+
+        // Complex setup.
+//        ArrayList<Predicate> world = new ArrayList<Predicate>(Arrays.asList(
+//                new Predicate(Predicate.ONTABLE, C),
+//                new Predicate(Predicate.ONTABLE, G),
+//                new Predicate(Predicate.CLEAR, A),
+//                new Predicate(Predicate.CLEAR, D),
+//                new Predicate(Predicate.ON, A, B),
+//                new Predicate(Predicate.ON, B, C),
+//                new Predicate(Predicate.ON, D, E),
+//                new Predicate(Predicate.ON, E, F),
+//                new Predicate(Predicate.ON, F, G),
+//                new Predicate(Predicate.ARMEMPTY)
+//        ));
+//
+//        ArrayList<Predicate> targetWorld = new ArrayList<Predicate>(Arrays.asList(
+//                new Predicate(Predicate.ONTABLE, A),
+//                new Predicate(Predicate.ONTABLE, F),
+//                new Predicate(Predicate.CLEAR, B),
+//                new Predicate(Predicate.CLEAR, E),
+//                new Predicate(Predicate.ON, B, D),
+//                new Predicate(Predicate.ON, D, G),
+//                new Predicate(Predicate.ON, G, A),
+//                new Predicate(Predicate.ON, E, C),
+//                new Predicate(Predicate.ON, C, F),
+//                new Predicate(Predicate.ARMEMPTY)
+//        ));
 
         System.out.println("From " + world);
         System.out.println("To " + targetWorld);
